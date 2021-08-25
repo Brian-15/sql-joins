e9abc4b21be8ed125e59619ce8e9c478330451ab
Brian-15 <bcirieco@gmail.com>
2021-08-24 20:48:39 -0700

    implemented queries for part 1

--- queries.sql
+++ queries.sql
@@ -1,1 +1,20 @@
--- write your queries here
+-- Part 1
+--
+SELECT * FROM owners o
+    LEFT JOIN vehicles v ON o.id = v.owner_id;
+
+--
+SELECT first_name, last_name, COUNT(*) AS count
+    FROM owners o JOIN vehicles v ON o.id = v.owner_id
+    GROUP BY o.id
+    ORDER BY first_name ASC;
+
+--
+SELECT first_name, last_name,
+    AVG(v.price) AS average_price,
+    COUNT(v.owner_id) AS count
+    FROM owners o JOIN vehicles v
+        ON o.id = v.owner_id
+    GROUP BY first_name, last_name
+        HAVING AVG(v.price) > 10000
+    ORDER BY first_name DESC;
